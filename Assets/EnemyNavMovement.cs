1da88b0f8d05a5c812daf244a4086588c93aca86
GAJ98 <gregoryalexisjohn@live.in>
2020-11-01 14:20:44 -0500

    Added basic Navmesh for ground

---
+++ Assets/EnemyNavMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyNavMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public GameObject player;
+
+    public ThirdPersonCharacter character;
+
+    [SerializeField]
+    float followRadius = 100f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(this.transform.position, player.transform.position) < followRadius)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, true);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            //increase time
+            Debug.Log("Hit Player");
+        }
+    }
+}
