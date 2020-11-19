e9ffe13488afca9f314888352f654f4dccbb2d7b
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2020-11-19 16:52:57 -0400

    minor addition to dash to merge with latest content

---
+++ Assets/Scripts/AbilitySystem/TriggerDash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDash : MonoBehaviour
+{
+    
+    [SerializeField] Transform player;
+
+    private Vector3 destination, startingPosition;
+    void Start()
+    {
+        startingPosition = player.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
