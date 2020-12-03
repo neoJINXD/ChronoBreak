ab6b75c5c9b65098da74190b37b4d649837556a9
GAJ98 <gregoryalexisjohn@live.in>
2020-12-03 01:23:11 -0500

    longer road and adjusted car movement

--- Assets/Scripts/Utils/ObstacleAnimation.cs
+++ Assets/Scripts/Utils/ObstacleAnimation.cs
@@ -3,47 +3,38 @@
 public class ObstacleAnimation : MonoBehaviour
 {
     // Assignables
-    [SerializeField]
-    Vector3 point1;
-
-    [SerializeField]
-    Vector3 point2;
-
-    [SerializeField]
-    float minSpeed;
-
-    [SerializeField]
-    float maxSpeed;
-
-    [SerializeField]
+    [SerializeField] Vector3 point1;
+    [SerializeField] Vector3 point2;
+    [SerializeField] float minSpeed;
+    [SerializeField] float maxSpeed;
     float speed;
-
-    float t;
+    float interpolant;
     bool increasing;
 
     void Start()
     {
-        t = Random.value;
-        increasing = Random.value > 0.5f;
+        interpolant = Random.value;
+        increasing = (Random.value > 0.5f);
         speed = Random.Range(minSpeed, maxSpeed);
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(point1, point2, t);
+        transform.position = Vector3.Lerp(point1, point2, interpolant);
 
         if (increasing)
         {
-            t += Time.deltaTime * speed;
-            if (t > 1.0f)
+            interpolant = interpolant + (Time.deltaTime * speed);
+            if (interpolant > 1.0f)
             {
                 increasing = false;
             }
         }
         else
         {
-            t -= Time.deltaTime * speed;
-            if (t < 0.0f)
+            interpolant = interpolant - (Time.deltaTime * speed);
+
+            if (interpolant < 0.0f)
             {
                 increasing = true;
             }
